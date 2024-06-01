4b1e046ebd6fa49bd2fa7e304498c6db8ff0f6ae
amang9446 <dev.aman7571@gmail.com>
2024-06-02 01:43:08 +0530

    fix cors declaration

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = require('./routes/todoRoutes')
 require('dotenv').config()
 const cors = require('cors')
-app.use(cors())
 const PORT = process.env.PORT
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
